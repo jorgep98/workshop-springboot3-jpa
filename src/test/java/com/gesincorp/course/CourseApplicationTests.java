c5e1d3379466190924a5c1bf3c2edcdef1a0925e
Jorge Perez <jorgeaperez8@gmail.com>
2022-12-06 17:16:34 -0400

    Project created

---
+++ src/test/java/com/gesincorp/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesincorp.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
